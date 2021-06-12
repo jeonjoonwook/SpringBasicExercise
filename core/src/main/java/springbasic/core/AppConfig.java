57185ef0454b7c9f86196fe727a0f5c54dd59a0a
jeonjoonwook <wook6139@naver.com>
2021-06-12 23:00:23 +0900

    ?

--- core/src/main/java/springbasic/core/AppConfig.java
+++ core/src/main/java/springbasic/core/AppConfig.java
@@ -1,5 +1,7 @@
 package springbasic.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springbasic.core.discount.DiscountPolicy;
 import springbasic.core.discount.FixDiscountPolicy;
 import springbasic.core.discount.RateDiscountPolicy;
@@ -10,20 +12,25 @@ import springbasic.core.member.MemoryMemberRepository;
 import springbasic.core.order.OrderService;
 import springbasic.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
